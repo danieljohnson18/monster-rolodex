bb6348efeb5fa0b217be4072d32223732453b1a4
Daniel Arikawe <47419174+danieljohnson18@users.noreply.github.com>
2020-07-31 11:28:59 +0100

    Updates

---
+++ precache-manifest.8b4dd54baf01b97c81af5315ecb6fcb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32416a94e84d5ed209f61abd19c9e5c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2a97eec37cc2c2ac54d1",
+    "url": "/monster-rolodex/static/css/main.14caa97e.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a97eec37cc2c2ac54d1",
+    "url": "/monster-rolodex/static/js/main.94732474.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
